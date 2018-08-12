ef0c477833387c208ccb944d8347896a647e474c
Ollie <reelfeet@hotmail.com>
2018-08-12 15:06:50 +0100

    Collage pictures

--- birthday/friends/templatetags/friend_extras.py
+++ birthday/friends/templatetags/friend_extras.py
@@ -1,4 +1,5 @@
 from django import template
+from math import ceil, sqrt
 
 register = template.Library()
 
@@ -9,3 +10,24 @@ def sort_by_total_points(queryset):
             queryset,
             key=lambda friend: friend.total_points,
             reverse=True)
+
+
+@register.simple_tag
+def sort_for_collage(queryset):
+    sorted_by_points = sort_by_total_points(queryset)
+    total_friends = len(sorted_by_points)
+    size = ceil(sqrt(total_friends))
+
+    count = 0
+    collage_friends = []
+    row_friends = []
+
+    for friend in sorted_by_points:
+        row_friends.append(friend)
+        count += 1
+        if count == size:
+            count = 0
+            collage_friends.append(row_friends)
+            row_friends = []
+
+    return collage_friends
