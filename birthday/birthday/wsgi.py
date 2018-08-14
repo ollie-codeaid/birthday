864d58757970e9143496b170a6deb98206c2875f
Ollie <reelfeet@hotmail.com>
2018-08-14 19:51:56 +0100

    Change settings

--- birthday/birthday/wsgi.py
+++ birthday/birthday/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "birthday.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "birthday.settings.production")
 
 application = get_wsgi_application()
